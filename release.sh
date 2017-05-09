3281e841d34b2680c84ac5962bf0b374b8e5f27d
Andreas Grapentin <andreas@grapentin.org>
2017-05-09 07:10:06 +0200

    initial commit - infrastructure to create release tarballs from icedove branding sources

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eu
+
+pkgname=icedove
+pkgver=53.0
+pkgrel=1
+
+srcdir=src
+distdir=${pkgname}-${pkgver}
+tarball=${pkgname}_${pkgver}-${pkgrel}.branding.tar.xz
+
+rm -rf $distdir/
+mkdir -pv $distdir
+
+cp -rv $srcdir/* $distdir/
+
+rm -f ${tarball}{,.sig}
+tar -cJf $tarball $distdir
+gpg --output ${tarball}.sig --detach-sig $tarball
+rm -rf $distdir
+
+echo "dist tarball:   $tarball"
+echo "dist signature: $tarball.sig"
